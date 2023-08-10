589fa1e1d6f4558a8ed04e97b9b15250d4587007
kggold4 <kggold4@gmail.com>
2023-08-10 10:31:24 +0300

    Updates

---
+++ precache-manifest.9338a0804cce4142ea58193eb67027c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9f3f1310687e92ac321",
+    "url": "/static/css/main.b860c20e.chunk.css"
+  },
+  {
+    "revision": "d9f3f1310687e92ac321",
+    "url": "/static/js/main.d9f3f131.chunk.js"
+  },
+  {
+    "revision": "699e1d39acb4ecda1fa3",
+    "url": "/static/js/1.699e1d39.chunk.js"
+  },
+  {
+    "revision": "4457e38924d3fc87d450",
+    "url": "/static/js/2.4457e389.chunk.js"
+  },
+  {
+    "revision": "e04dd1ab913315a963d0",
+    "url": "/static/js/runtime~main.e04dd1ab.js"
+  },
+  {
+    "revision": "61df4a42125383d83fc2814379a018e3",
+    "url": "/index.html"
+  }
+];
